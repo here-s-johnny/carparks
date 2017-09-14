5f7bbe5f9e71f94541ebf4e24f6b36e24d90a2b1
Jan Gajl <johnny@johnnys-macbook.local>
2017-09-14 09:19:31 +0200

    refactoring

--- src/main/java/carparks/resources/CarParkController.java
+++ src/main/java/carparks/resources/CarParkController.java
@@ -1,28 +1,28 @@
-package carparks.controllers;
+package carparks.resources;
 
 import java.time.LocalDate;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class FeesController {
 	
-	// --------------- Retrieve a fee --------------------- //
-	@RequestMapping(value = "/fee/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> getFee(@PathVariable("id") long id) {
+	// --------------- Retrieve a parking record --------------------- //
+	@GetMapping(value = "/parking/{id}")
+	public ResponseEntity<?> getParking(@PathVariable("id") long id) {
 		return new ResponseEntity<Long>(id, HttpStatus.OK);
 	}
 	
-	// --------------- Create a fee ---------------------- //
-	@RequestMapping(value = "/fee", method = RequestMethod.POST)
-	public ResponseEntity<?> createFee(@RequestBody String plateNumber) {
+	// --------------- Create a parking entity ---------------------- //
+	@PostMapping(value = "/parking")
+	public ResponseEntity<?> createParking(@RequestBody String plateNumber) {
 //		HttpHeaders headers = new HttpHeaders();
 //        headers.setLocation(ucBuilder.path("/fee/{id}").buildAndExpand(fee.getId()).toUri());
 //        return new ResponseEntity<String>(headers, HttpStatus.CREATED);
@@ -30,15 +30,14 @@ class FeesController {
 		return new ResponseEntity<String>("new fee", HttpStatus.CREATED);
 	}
 	
-	
-	// -------------- Update a fee ------------------------ //
-	@RequestMapping(value = "/fee/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<?> updateFee(@PathVariable("id") long id) {
+	// -------------- Update a parking entity ------------------------ //
+	@PutMapping(value = "/parking/{id}")
+	public ResponseEntity<?> updateParking(@PathVariable("id") long id) {
 		return new ResponseEntity<String>("new fee", HttpStatus.CREATED);
 	}
 	
 	// ---- Retrieve the sum of all fees in a given day ---- //
-	@RequestMapping(value = "/fees", method = RequestMethod.GET)
+	@GetMapping(value = "/fees")
 	public ResponseEntity<?> getFeesSum(@RequestBody LocalDate date) {
 		return new ResponseEntity<String>("sum", HttpStatus.OK);
 	}
